4f277aee0503f1999aa8eeb04fa856f23e12444c
Your Name <you@example.com>
2022-07-12 09:06:43 -0300

    proj

---
+++ questao1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            while(x != 2002)
+            {
+                Console.WriteLine("Senha Inválida");
+                x = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
